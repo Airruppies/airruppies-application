ee5a8f73cf1e1ee73658b8f0e4bb0b44dfb3366d
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2024-10-20 07:15:51 +0100

    Refactored

--- services/notification/src/main/java/com/airruppies/airruppiesapp/email/EmailService.java
+++ services/notification/src/main/java/com/airruppies/airruppiesapp/email/EmailService.java
@@ -40,7 +40,7 @@ public class EmailService {
 
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, UTF_8.name());
-        messageHelper.setFrom("contact@aliboucoding.com");
+        messageHelper.setFrom("airruppies@gmail.com");
 
         final String templateName = PAYMENT_CONFIRMATION.getTemplate();
 
